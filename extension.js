0d1e33ae903bbceb5798713f66508eb0dd688bff
OrelDiei <golala58@gmail.com>
2024-04-08 20:29:14 +0300

    Syntax Checker
    Checking if the Syntax of the file and the file itself is HDL

--- extension.js
+++ extension.js
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const hdl_parser = require('hdl-parser');
+const path = require("path");
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -16,15 +18,35 @@ function activate(context) {
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('hdl-diagram.helloWorld', function () {
+
+	let extensionExcute = vscode.commands.registerCommand('nand2tetris-hdl-visualizer.visualize', function () {
 		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from hdl-diagram!');
+		// get paths of current document and directory
+		let doc = vscode.window.activeTextEditor.document;
+		let doc_path = doc.uri.fsPath;
+		let dir_path = path.dirname(doc_path);
+
+		// check if document is hdl file
+		if (!doc_path.endsWith(".hdl")) {
+			vscode.window.showInformationMessage("Error: Input file does not have the .hdl extension.");
+			return;
+		}		
+		
+		// parse hdl file and check if the file has proper hdl syntax
+		let doc_phdl;
+		let doc_text = doc.getText();
+		try {
+			doc_phdl = hdl_parser.parse(doc_text);
+		}
+		catch(err) {
+			vscode.window.showInformationMessage(err.toString());
+			return;
+		}
 	});
 
-	context.subscriptions.push(disposable);
+
+	context.subscriptions.push(extensionExcute);
 }
 
 // This method is called when your extension is deactivated
@@ -33,4 +55,4 @@ function deactivate() {}
 module.exports = {
 	activate,
 	deactivate
-}
+};
