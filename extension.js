c0c5fe40783966fefac1ebb88776aa024e4345af
OrelDiei <golala58@gmail.com>
2024-04-15 19:31:58 +0300

    update 15.4.2024
    syntax checker: fixed bug now works completly
    starting to analyze user's code viz hdl parser

--- extension.js
+++ extension.js
@@ -2,8 +2,8 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 const hdl_parser = require('hdl-parser');
-const path = require("path");
-
+// const path = require('path');
+// const path = require('path');
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 
@@ -18,35 +18,56 @@ function activate(context) {
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('hdl-diagram.helloWorld', function () {
 
-	let extensionExcute = vscode.commands.registerCommand('nand2tetris-hdl-visualizer.visualize', function () {
-		// The code you place here will be executed every time your command is executed
+		let userCode = vscode.window.activeTextEditor.document;
+		let textPath = userCode.uri.fsPath;
+		// let dirPath = path.dirname(textPath);
 
-		// get paths of current document and directory
-		let doc = vscode.window.activeTextEditor.document;
-		let doc_path = doc.uri.fsPath;
-		let dir_path = path.dirname(doc_path);
-
-		// check if document is hdl file
-		if (!doc_path.endsWith(".hdl")) {
-			vscode.window.showInformationMessage("Error: Input file does not have the .hdl extension.");
+		//checking to see if the user's code file ends with .hdl
+		if (!textPath.endsWith(".hdl")){
+			vscode.window.showInformationMessage("Error: this file does not ends with .hdl");
 			return;
-		}		
-		
-		// parse hdl file and check if the file has proper hdl syntax
-		let doc_phdl;
-		let doc_text = doc.getText();
-		try {
-			doc_phdl = hdl_parser.parse(doc_text);
 		}
-		catch(err) {
+
+		let parsedUserText;
+		let userCodeText = userCode.getText();
+
+		// checking if the user's code have some Syntax errors
+		try{
+			parsedUserText = hdl_parser.parse(userCodeText);
+			
+		}catch(err){
 			vscode.window.showInformationMessage(err.toString());
-			return;
+			return
 		}
+
+		// the data of the chip itself, that "parts" will be analyze after
+		let INmainPin = [];
+		let OUTmainPin = [];
+		for (let i = 0; i < parsedUserText.definitions.length; i++) {
+			const definition = parsedUserText.definitions[i];
+		
+			if (definition.type === "IN") {
+				for (let j = 0; j < definition.pins.length; j++) {
+					INmainPin.push(definition.pins[j].name);
+				}
+			}
+		
+			if (definition.type === "OUT") {
+				for (let j = 0; j < definition.pins.length; j++) {
+					OUTmainPin.push(definition.pins[j].name);
+				}
+			}
+		}
+
+		// debug output to check the analyze, just for us
+		vscode.window.showInformationMessage(INmainPin.toString());
+		vscode.window.showInformationMessage(OUTmainPin.toString());
 	});
 
-
-	context.subscriptions.push(extensionExcute);
+	context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
@@ -55,4 +76,4 @@ function deactivate() {}
 module.exports = {
 	activate,
 	deactivate
-};
+}
